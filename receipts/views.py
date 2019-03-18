871e564098427ad1fd4950fe3c399e022f834ac4
yasuyasu-ot <y.otsubo.dss@gmail.com>
2019-03-19 02:33:01 +0900

    [fix] create時に画像解析対応

--- receipts/views.py
+++ receipts/views.py
@@ -6,12 +6,13 @@ from django.core.exceptions import PermissionDenied
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from .models import Receipt
-from .forms import ReceiptModelForm
 
+from PIL import Image
 import numpy as np
 import requests
 import json
 import base64
+import cv2
 
 """
 Django Auth
@@ -33,10 +34,6 @@ class IndexView2(generic.ListView):
     ordering = ['-updated_at']
     template_name = 'receipt/receipt_list.html'
 
-    # def get_queryset(self):
-    #     userid = self.request.user
-    #     return Receipt.objects.filter(author_id=userid)
-
 
 class UpdateView2(LoginRequiredMixin, generic.edit.UpdateView):  # The LoginRequired mixin
     model = Receipt
@@ -51,9 +48,7 @@ class UpdateView2(LoginRequiredMixin, generic.edit.UpdateView):  # The LoginRequ
         obj = self.get_object()
         if obj.author != self.request.user:
             raise PermissionDenied('You do not have permission to edit.')
-
         return super(UpdateView2, self).dispatch(request, *args, **kwargs)
-
 
 
 class DetailView2(generic.DetailView):
@@ -73,14 +68,10 @@ class CreateView2(LoginRequiredMixin, generic.edit.CreateView):  # The LoginRequ
         # It should return an HttpResponse.
         # https://docs.djangoproject.com/en/2.0/topics/class-based-views/generic-editing/#models-and-request-user
         form.instance.author = self.request.user
-        # analyze_imgage = form.cleaned_data['image']
-        # immage_array = np.asarray(Image.open(analyze_imgage)
-        # image_path = form.instance.image.path
-        # analyze_text = text_detection(image_path)
-        # print(image_path)
-        # print(analyze_text)
-        # form.instance.sum_total()
-        #        form.instance.sum_total = 3333
+        analyze_imgage = form.cleaned_data['image']
+        analyze_json = text_detection(analyze_imgage)
+        description = analyze_json['responses'][0]['textAnnotations'][0]['description']
+        form.instance.text = description
 
         return super(CreateView2, self).form_valid(form)
 
@@ -102,36 +93,37 @@ class DeleteView2(LoginRequiredMixin, generic.edit.DeleteView):  # The LoginRequ
         return super(DeleteView2, self).dispatch(request, *args, **kwargs)
 
 
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        # https://docs.djangoproject.com/en/2.0/topics/class-based-views/generic-editing/#models-and-request-user
+        analyze_imgage = form.cleaned_data['image']
+        analyze_json = text_detection(analyze_imgage)
+        description = analyze_json['responses'][0]['textAnnotations'][0]['description']
+        form.instance.text = description
+
+        return super(C2, self).form_valid(form)
+
+
 def text_detection(image_path):
-    API_KEY = "AIzaSyAD2HjhCEDFEIETt78Ak3UQf2dlVnND_CY"
+    # 注意
+    API_KEY = "AIzaSyAj9Gwi_5JkYkgyZp-NiLPRvW57BK9gq7Q"
     api_url = 'https://vision.googleapis.com/v1/images:annotate?key={}'.format(API_KEY)
-    with open(image_path, "rb") as img:
-        image_content = base64.b64encode(img.read())
-        req_body = json.dumps({
-            'requests': [{
-                'image': {
-                    'content': image_content.decode('utf-8')  # base64でエンコードしたものjsonにするためdecodeする
-                },
-                'features': [{
-                    'type': 'TEXT_DETECTION'
-                }]
+
+    immage_array = np.asarray(Image.open(image_path))
+
+    result, dst_data = cv2.imencode('.jpg', immage_array)
+    image_content = base64.b64encode(dst_data)
+
+    req_body = json.dumps({
+        'requests': [{
+            'image': {
+                'content': image_content.decode('utf-8')  # base64でエンコードしたものjsonにするためdecodeする
+            },
+            'features': [{
+                'type': 'TEXT_DETECTION'
             }]
-        })
-        res = requests.post(api_url, data=req_body)
-        return res.json()
-
-    # if __name__ == "__main__":
-    #     img_path = "画像のpath"
-    #     res_json = text_detection(img_path)
-    #     res_text = res_json["responses"][0]["textAnnotations"][0]["description"]
-    #     # print(json.dumps(res_json, indent=4, sort_keys=True, ensure_ascii=False))
-    #     print(res_text)
-    #     with open("result.json", "w") as js:
-    #         # json.dump(res_json, js, indent=4, ensure_ascii=False)
-    #         js.write(res_text)
-
-#
-# class Upload(generic.CreateView):
-#     model = Receipt
-#     form_class = ReceiptModelForm
-#     success_url = '/receipt'
+        }]
+    })
+    res = requests.post(api_url, data=req_body)
+    return res.json()
